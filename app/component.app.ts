4aa687371a493241b4a5f36887a095dd0e419440
Wendy <honeykatie@qq.com>
2017-02-18 22:21:28 -0800

    Finish Chapter 2 3-5

--- app/component.app.ts
+++ app/component.app.ts
@@ -2,12 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  templateUrl: './partials/app.html' 
+  templateUrl: './partials/app.html'
 })
 
 export class AppComponent {
   name: string;
   artists: any;
+
+  onClick(myItem, myElement){
+    // console.log(e.target); // e <= $event in (click) = "onClick($())"
+    this.name = myItem.name;
+    myElement.style.backgroundColor = "#FE04AE"
+    console.log(this.name);
+    console.log(myElement);
+  }
+
+  addArtist(value, e){
+      this.artists.push({
+        name: value,
+        school: "SAP Next Gen"
+      });
+      console.log(JSON.stringify(this.artists));
+      console.log(e.target);
+  }
 
   constructor() {
     this.name = 'Xhou Ta';
@@ -21,7 +38,7 @@ export class AppComponent {
       }, {
         name: 'Hillary Post',
         school: 'University of Florida'
-      } 
+      }
     ]
   }
 }
